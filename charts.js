6270dea31698c1c3dee720dec7b4272fd06c7f66
Apollo619 <ryan48meredith@yahoo.com>
2021-05-27 19:02:02 -0500

    module 12 bubble chart

--- charts.js
+++ charts.js
@@ -109,7 +109,31 @@ function buildCharts(sample) {
     };
   // 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot("bar", barData, barLayout)
+
+      // 1. Create the trace for the bubble chart.
+    var bubbleData = [{
+      x: otu_ids,
+      y: sample_values,
+      mode: 'markers',
+      text: otu_labels,
+      marker: {
+        size: sample_values,
+        color: otu_ids,
+        colorscale: "earth"
+      }
+    }];
+
+      // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      title: "Bacteria Cultures Per Sample",
+      showlegend: false,
+      sizemode: 'area',
+      xaxis: {title: "OTU ID"},
+
+    };
+      console.log(bubbleData);
+      // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
+
   });
 }
-
-
